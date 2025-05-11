12263fc4eb88c2ffbc7274888f7adfb73602ee08
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-11 15:49:51 +0900

    Add content generation module initialization

---
+++ src/content_generation/__init__.py
@@ -0,0 +1,10 @@
+"""
+Content Generation Module
+
+This module provides functionality for generating various types of content
+(images, narratives, music) from dream data using AI models.
+"""
+
+from .generator import ContentGenerator, DreamContent
+
+__all__ = ['ContentGenerator', 'DreamContent']
